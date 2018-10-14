521e9e7c9699163329d82c00d0f83daab739f94a
Mark Wardell <mark.d.wardell@avanade.com>
2018-10-14 16:40:43 -0500

    renamed and moved Items to Different Folders

--- KattisUtilities/HTML/KattisFromProblemItem.cs
+++ KattisUtilities/HTML/KattisFromProblemItem.cs
@@ -30,16 +30,7 @@ namespace KattisUtilities.HTML
             ID = id;
             MetricsDictionary = dict;
             
-            //XElement myTable = xdoc.Descendants("table").FirstOrDefault(xelem => xelem.Attribute("class").Value == "inner");
-            //IEnumerable<IEnumerable<XElement>> myRows = myTable.Elements().Select(xelem => xelem.Elements());
-
-            //foreach (IEnumerable<XElement> tableRow in myRows)
-            //{
-            //    foreach (XElement rowCell in tableRow)
-            //    {
-            //        // tada..
-            //    }
-            //}
+          
 
         }
     }
