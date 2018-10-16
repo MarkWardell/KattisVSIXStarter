2a7f9f044b84b1c7e2f20674998e02c298d43cea
Mark Wardell <mark.d.wardell@avanade.com>
2018-10-15 21:27:28 -0500

    Got URL

--- KattisClasses/KattisUtilities/HTML/NextProblemScrape.cs
+++ KattisClasses/KattisUtilities/HTML/NextProblemScrape.cs
@@ -14,15 +14,8 @@ namespace KattisUtilities.HTML
             HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
             doc.LoadHtml(page);
             var findAnchors = doc.DocumentNode.Descendants("a");
-            var n = findAnchors.First();
-            var o = findAnchors.Last();
-            List<string> strList = new List<string>();
-            var node = findAnchors.Where(d => d.InnerText.Contains("Next")).First();
-           
+            var node = findAnchors.Where(d => d.InnerText.Contains("Next")).First();   
             string hrefValue = node.GetAttributeValue("href", String.Empty);
-
-
-
             return hrefValue;
         }
     }
